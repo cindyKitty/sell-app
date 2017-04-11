09c1215979b26c26034070de3a91b4bbc2c413dc
cindyKitty <sagajy@163.com>
2017-04-11 21:45:17 +0800

    将页面拆分为header、tab、content三个组件，并实现组件间的调用；编写stylus样式需载入stylus和stylus-loader两个依赖

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,1 +1,13 @@
-header.vue
+<template>
+  <div class="header">
+  	我是header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default{}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	
+</style>
