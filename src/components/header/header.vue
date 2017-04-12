dfb61b050268018f6beac7c2dd654113c934cb03
cindyKitty <sagajy@163.com>
2017-04-12 15:30:20 +0800

    商品页_公共及优惠信息，实现弹层

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,17 +17,20 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count"  @click="detailShow=true">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-  	<div class="bulletin-wrapper">
+  	<div class="bulletin-wrapper"  @click="detailShow=true">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div class="detail" v-show="detailShow">
+      
     </div>
   </div>
 </template>
@@ -37,6 +40,11 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data () {
+      return {
+        detailShow: false
       }
     },
     created () {
@@ -50,6 +58,7 @@
 
   .header
     position: relative
+    overflow: hidden
     color: #fff
     background: rgba(7, 17, 27, 0.5)
     .content-wrapper
@@ -133,7 +142,7 @@
       .bulletin-title
         display: inline-block
         vertical-align: top
-        margin-top: 7px
+        margin-top: 8px
         width: 22px
         height: 12px
         bg-image('bulletin')
@@ -156,4 +165,13 @@
       height: 100%
       z-index: -1
       filter: blur(10px)
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7, 17, 27, 0.8)
 </style>
