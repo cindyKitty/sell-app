6396ae67570540ad1d89ef775f8f99c155cba607
cindyKitty <sagajy@163.com>
2017-04-14 01:39:07 +0800

    购物车小球动画实现

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -26,6 +26,7 @@
         } else {
           this.food.count += 1
         }
+        this.$dispatch('cart.add', event.target)
       },
       decreaseCart(event) {
         if (!event._constructed) {
@@ -45,7 +46,7 @@
       transition: all 0.4s linear
       &.move-transition
         opacity: 1
-        transform: translate3D(0, 0, 0)
+        transform: translate3d(0, 0, 0)
         .inner
           display: inline-block
           padding: 6px
@@ -56,7 +57,7 @@
           transform: rotate(0)
       &.move-enter,&.move-leave
         opacity:0
-        transform: translate3D(24px,0,0)
+        transform: translate3d(24px,0,0)
         .inner
       	  transform: rotate(180deg)
     .cart-count
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -35,7 +35,7 @@
       	</li>
       </ul>
     </div>
-    <shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <shopcart v-ref:shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
@@ -69,7 +69,6 @@
         return 0
       },
       selectFoods() {
-        debugger
         let foods = []
         this.goods.forEach((good) => {
           good.foods.forEach((food) => {
@@ -124,11 +123,23 @@
           height += item.clientHeight
           this.listHeight.push(height)
         }
+      },
+      _drop(target) {
+        // 体验优化，异步执行下落动画
+        this.$nextTick(() => {
+          // 访问子组件的方法
+        this.$refs.shopcart.drop(target)
+      })
       }
     },
     components: {
       shopcart,
       cartcontrol
+    },
+    events: {
+      'cart.add' (target) {
+        this._drop(target)
+      }
     }
   }
 </script>
