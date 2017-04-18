eedd294cb0c360e2bf824db4deea34a9a5517cd3
cindyKitty <sagajy@163.com>
2017-04-18 14:21:12 +0800

    商品页--收藏布局样式实现；并实现交互逻辑

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -28,6 +28,9 @@
   	  	  	</div>
   	  	  </li>
   	  	</ul>
+  	  	<div class="favorite" @click="toggleFavorite">
+  	  	  <span class="icon-favorite" :class="{'active': favorite}"></span><span class="text">{{favoriteText}}</span>
+  	  	</div>
   	  </div>
   	  <split></split>
   	  <div class="bulletin">
@@ -72,6 +75,16 @@
         type: Object
       }
     },
+    data() {
+      return {
+        favorite: false
+      }
+    },
+    computed: {
+      favoriteText() {
+        return this.favorite ? '已收藏' : '收藏'
+      }
+    },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
     },
@@ -86,6 +99,12 @@
       }
     },
     methods: {
+      toggleFavorite(event) {
+        if (!event._constructed) {
+          return
+        }
+        this.favorite = !this.favorite
+      },
       _initScroll() {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$els.seller, {click: true})
@@ -128,6 +147,7 @@
     width: 100%
     overflow: hidden
     .overview
+      position: relative
       padding: 18px
       .title
         margin-bottom: 8px
@@ -169,6 +189,24 @@
           	color: rgb(7,17,27)
           	.stress
           	  font-size: 24px
+      .favorite
+      	position: absolute
+      	width: 50px
+      	top: 18px
+      	right: 5px
+      	text-align: center
+      	.icon-favorite
+      	  display: block
+      	  margin-bottom: 4px
+      	  line-height: 24px
+      	  font-size: 24px
+      	  color: #d4d6d9
+      	  &.active
+      	  	color: rgb(240,20,20)
+      	.text
+      	  line-height: 10px
+      	  font-size: 10px
+      	  color: rgb(77,85,93)
     .bulletin
       padding: 18px 18px 0
       .title
