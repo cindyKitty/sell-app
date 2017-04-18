94e1befcdfe7c68e2f940a1b82a3cbe3d95c2fc0
cindyKitty <sagajy@163.com>
2017-04-18 13:09:44 +0800

    商家页-公告与活动区域结构定义、样式布局实现；并应用better-scroll实现内容区滚动

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -29,20 +29,54 @@
   	  	  </li>
   	  	</ul>
   	  </div>
+  	  <split></split>
+  	  <div class="bulletin">
+  	  	<h1 class="title">公告与活动</h1>
+  	  	<p class="content border-1px">{{seller.bulletin}}</p>
+  	  	<ul v-if="seller.supports" class="supports">
+  	  	  <li class="support-item border-1px" v-for="item in seller.supports">
+  	  	  	<span class="icon" :class="classMap[item.type]"></span>
+  	  	  	<span class="text">{{item.description}}</span>
+  	  	  </li>
+  	  	</ul>
+  	  </div>
   	</div>
   </div>
 </template>
 
 <script>
+  import BScroll from 'better-scroll'
   import star from 'components/star/star'
+  import split from 'components/split/split'
   export default {
     props: {
       seller: {
         type: Object
       }
     },
+    created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    ready() {
+      this._initScroll()
+    },
+    watch: {
+      'seller'() {
+        this._initScroll()
+      }
+    },
+    methods: {
+      _initScroll() {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$els.seller, {click: true})
+        } else {
+          this.scroll.refresh()
+        }
+      }
+    },
     components: {
-      star
+      star,
+      split
     }
   }
 </script>
@@ -98,4 +132,45 @@
           	color: rgb(7,17,27)
           	.stress
           	  font-size: 24px
+    .bulletin
+      padding: 18px 18px 0
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        font-size: 14px
+        color: rgb(7,17,27)
+      .content
+      	padding: 0 12px 16px
+      	border-1px(rgba(7,17,27,0.1))
+      	line-height: 24px
+      	font-size: 12px
+      	color: rgb(240,20,20)
+      .support-item
+      	padding: 16px 12px
+      	border-1px(rgba(7,17,27,0.1))
+      	font-size: 0
+      	&:last-child
+      	  border-none()
+      	.icon
+          display: inline-block
+          width: 16px
+          height: 16px
+          vertical-align: top
+          margin-right: 6px
+          background-size: 16px 16px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_4')
+          &.discount
+            bg-image('discount_4')
+          &.guarantee
+            bg-image('guarantee_4')
+          &.invoice
+            bg-image('invoice_4')
+          &.special
+            bg-image('special_4')
+        .text
+          line-height: 16px
+          font-size: 12px
+          color: rgb(7,17,27)
 </style>
